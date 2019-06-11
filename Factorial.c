04d26e565efcf8efaf4d5e2a9403042027ba55c2
Bharathwaj3003 <51689385+Bharathwaj3003@users.noreply.github.com>
2019-06-11 09:59:49 +0530

    Update Factorial.c

--- Factorial.c
+++ Factorial.c
@@ -4,10 +4,13 @@ int main()
   long long int fact=1;
   int n;
   scanf("%d",&n);
+  if(n>20)
+    return 0;
   while(n>0)
   {
     fact=fact*n;
     n--;
   }
   printf("%lld",fact);
+  return 0;
 }
